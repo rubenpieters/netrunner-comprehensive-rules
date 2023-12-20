b700a60f2ed3dc0ee63f664b7fc60d9395d3ef66
rubenpieters <rubenpietersgithub@gmail.com>
2023-12-20 20:05:28 +0100

    update run script

--- scripts/run_web_server_docker.sh
+++ scripts/run_web_server_docker.sh
@@ -1,2 +1,2 @@
-docker build -t phpserver:latest .
-docker run -p 80:80 -v ${PWD}:/workdir phpserver:latest /workdir/scripts/run_web_server.sh
+# build the docker image: docker build -t phpserver:latest .
+docker run --rm -p 80:80 -v ${PWD}:/workdir phpserver:latest /workdir/scripts/run_web_server.sh
